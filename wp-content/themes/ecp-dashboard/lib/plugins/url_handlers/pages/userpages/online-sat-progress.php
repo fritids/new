321b42bb9b0eb399fb34df48808c4ce67c06afdb
David Bergmann <davidbepa@gmail.com>
2012-12-26 15:24:46 -0500

    remove accordion for progress table

--- wp-content/themes/ecp-dashboard/lib/plugins/url_handlers/pages/userpages/online-sat-progress.php
+++ wp-content/themes/ecp-dashboard/lib/plugins/url_handlers/pages/userpages/online-sat-progress.php
@@ -12,40 +12,40 @@ include dirname(__FILE__)."/../menues/menu_student.php";
 ?>
 <div class="whitebox">
 <script type="text/javascript">
-	var openedList;
-	jQuery(function(){
-		jQuery("#onlineSATprogress li ul").hide();
-		jQuery("#onlineSATprogress li").not("table a").click(function(e){
-			if(jQuery(e.target).hasClass("review")){ return true; }
-			//if(!jQuery(this).hasClass("review")){
-				e.preventDefault();
-				e.stopPropagation();
-	
-					if(jQuery(this).find("a:first").hasClass("expanded")){
-						jQuery(this).find("a:first").removeClass("expanded");
-						var toOpen=jQuery(this).find("ul:first");
-						toOpen.addClass("opened").slideUp();
-					}else{
-	
-						jQuery(this).parent().find("a.expanded").trigger("click")
-	
-						jQuery(this).find("a:first").addClass("expanded");
-	
-						if(jQuery(this).parents(".opened").length==0){
-							if(openedList!=null){
-								openedList.slideUp();
-							}
-							var toOpen=openedList=jQuery(this).find("ul:first");
-							toOpen.addClass("opened").slideDown();
-						}else{
-							var toOpen=jQuery(this).find("ul:first");
-							toOpen.addClass("opened").slideDown();
-						}
-					}
-			//}
-		})
-		//jQuery("#onlineSATprogress li .review").unbind("click");
-	})
+//	var openedList;
+//	jQuery(function(){
+//		jQuery("#onlineSATprogress li ul").hide();
+//		jQuery("#onlineSATprogress li").not("table a").click(function(e){
+//			if(jQuery(e.target).hasClass("review")){ return true; }
+//			//if(!jQuery(this).hasClass("review")){
+//				e.preventDefault();
+//				e.stopPropagation();
+//	
+//					if(jQuery(this).find("a:first").hasClass("expanded")){
+//						jQuery(this).find("a:first").removeClass("expanded");
+//						var toOpen=jQuery(this).find("ul:first");
+//						toOpen.addClass("opened").slideUp();
+//					}else{
+//	
+//						jQuery(this).parent().find("a.expanded").trigger("click")
+//	
+//						jQuery(this).find("a:first").addClass("expanded");
+//	
+//						if(jQuery(this).parents(".opened").length==0){
+//							if(openedList!=null){
+//								openedList.slideUp();
+//							}
+//							var toOpen=openedList=jQuery(this).find("ul:first");
+//							toOpen.addClass("opened").slideDown();
+//						}else{
+//							var toOpen=jQuery(this).find("ul:first");
+//							toOpen.addClass("opened").slideDown();
+//						}
+//					}
+//			//}
+//		})
+//		//jQuery("#onlineSATprogress li .review").unbind("click");
+//	})
 </script>
 <div class="table-holder" id="onlineSATprogress">
 <?php 
@@ -56,33 +56,16 @@ include dirname(__FILE__)."/../menues/menu_student.php";
 	?>
 		<ul>
         	<li>
-	            <a class='tableslider levelone collapsed'>Reading</a>
-	            <?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Reading"),$user_id);echo $wpm->toString(); ?>
+	            <a class='tableslider levelone collapsed'>SAT & ACT</a>
+	            <?php $wpm=new Wp_Menu(wp_get_nav_menu_items("SAT & ACT"),$user_id);echo $wpm->toString(); ?>
            	</li>
            	<li>
-	            <a class='tableslider levelone collapsed'>Math</a>
-	            <ul>
-        			<li>
-        				<a class='tableslider levelone collapsed'>Arithmetic</a>
-		            	<?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Math - Arithmetic"),$user_id);echo $wpm->toString(); ?>
-		            </li>
-		            <li>
-        				<a class='tableslider levelone collapsed'>Algebra</a>
-		            	<?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Math - Algebra"),$user_id);echo $wpm->toString(); ?>
-		            </li>
-		            <li>
-        				<a class='tableslider levelone collapsed'>Geometry</a>
-		            	<?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Math - Geometry"),$user_id);echo $wpm->toString(); ?>
-		            </li>
-		            <li>
-        				<a class='tableslider levelone collapsed'>Advanced Topics</a>
-		            	<?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Math - Advanced Topics"),$user_id);echo $wpm->toString(); ?>
-		            </li>
-		        </ul>
+	            <a class='tableslider levelone collapsed'>SAT Only</a>
+	            <?php $wpm=new Wp_Menu(wp_get_nav_menu_items("SAT Only"),$user_id);echo $wpm->toString(); ?>
            	</li>
            	<li>
-	            <a class='tableslider levelone collapsed'>Writing</a>
-	            <?php $wpm=new Wp_Menu(wp_get_nav_menu_items("Writing"),$user_id);echo $wpm->toString(); ?>
+	            <a class='tableslider levelone collapsed'>ACT Only</a>
+	            <?php $wpm=new Wp_Menu(wp_get_nav_menu_items("ACT Only"),$user_id);echo $wpm->toString(); ?>
            	</li>
           </ul>
 
