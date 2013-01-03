409e56b05095d049346d409edaba09339aa14604
David Bergmann <davidbepa@gmail.com>
2013-01-03 16:18:45 -0500

    profile changes

--- wp-content/themes/ecp-dashboard/lib/plugins/url_handlers/pages/userpages/profile.php
+++ wp-content/themes/ecp-dashboard/lib/plugins/url_handlers/pages/userpages/profile.php
@@ -65,6 +65,7 @@ $sections=get_user_meta($user_id,"_IDGL_elem_user_type",true);
 	        <input type="submit" class="button green right" value="Update Profile">
 	    </div>
         <?php else: ?>
+	
      <div class="split">   
         <div class="formline"><span class="label">First Name:</span> <input name="first_name" type="text" value="<?php echo $userProfile->first_name; ?>" /></div>
         <div class="formline"><span class="label">Last Name:</span> <input name="last_name" type="text" value="<?php echo $userProfile->last_name; ?>" /></div>
@@ -96,35 +97,114 @@ $sections=get_user_meta($user_id,"_IDGL_elem_user_type",true);
 		<br>
 		<div class="formline checks"><?php IDGL_Users::IDGL_renderField($userProfile,"recieve_info"); ?></div>
 		<br>
-		<h4>SAT Scores:</h4>
+		<h4>PSAT Scores:</h4>
 		<div class="formline sat-scores">
 			<?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Reading"); ?>
 			<?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Math"); ?>
 			<?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Writing"); ?>
 		</div>
-		
+		<br>
 		<h4>Test Dates:</h4>
-        <div class="formline hiddeen_label"><span class="label">Add Test Date</span>
-            <?php IDGL_Users::IDGL_renderField($userProfile,"Test_Date_1"); ?>
+		<div class="label">Add SAT Test Date:</div><br><br>
+        <div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_1"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_1_Reading"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_1_Math"); ?>
+			<span class="label">Writing</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_1_Writing"); ?>
         </div>
-		<div class="formline hiddeen_label"><span class="label">Add Test Date</span>
-            <?php IDGL_Users::IDGL_renderField($userProfile,"Test_Date_2"); ?>
+		<div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_2"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_2_Reading"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_2_Math"); ?>
+			<span class="label">Writing</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_2_Writing"); ?>
         </div>
-		<div class="formline hiddeen_label"><span class="label">Add Test Date</span>
-            <?php IDGL_Users::IDGL_renderField($userProfile,"Test_Date_3"); ?>
+		<div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_3"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_3_Reading"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_3_Math"); ?>
+			<span class="label">Writing</span><?php IDGL_Users::IDGL_renderField($userProfile,"SAT_Date_3_Writing"); ?>
         </div>
-		<div class="formline hiddeen_label"><span class="label">Add Test Date</span>
-            <?php IDGL_Users::IDGL_renderField($userProfile,"Test_Date_4"); ?>
+		<br><div class="label">Add ACT Test Date:</div><br><br>
+		<div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_1"); ?>
+			<span class="label">English</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_1_English"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_1_Math"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_1_Reading"); ?>
+			<span class="label">Science</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_1_Science"); ?>
+        </div>
+		<div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_2"); ?>
+			<span class="label">English</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_2_English"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_2_Math"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_2_Reading"); ?>
+			<span class="label">Science</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_2_Science"); ?>
+        </div>
+		<div class="formline hiddeen_label test_dates">
+            <?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_3"); ?>
+			<span class="label">English</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_3_English"); ?>
+			<span class="label">Math</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_3_Math"); ?>
+			<span class="label">Reading</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_3_Reading"); ?>
+			<span class="label">Science</span><?php IDGL_Users::IDGL_renderField($userProfile,"ACT_Date_3_Science"); ?>
         </div>
 		<hr>
 		
-		<div class="bluebox">
-            <label><strong>Change Password</strong></label>
-            <?php if($pass_note) echo "<p style='color:#fff;'>".$pass_note."</p>"; ?>
-            <label><span>Old Password:</span> <input name="o_pass" type="password" value="" /></label>
-            <label><span>New Password:</span> <input name="n_pass" type="password" value="" /></label>
-            <label><span>Repeat New Password:</span> <input name="n_c_pass" type="password" value="" /></label>
-        </div>
+		<div class="two-column clearfix">
+			<div class="column">
+				<div class="purchase-info clearfix">
+					<label><strong>Purchase Info</strong></label>
+					<div class="column">
+						<div class="label">Products:</div>
+						<?php
+						$products=unserialize(get_user_meta($user_id,"_IDGL_elem_ECP_user_order",true));
+
+						switch_to_blog(3);
+
+						$query = new WP_Query( array(
+							'posts_per_page'=>-1,
+							'post_type'=>'ecpproduct'
+						));
+						echo "<ul>";
+						while ( $query->have_posts() ) : $query->the_post(); global $post;
+							if( in_array($post->ID, $products)){
+								echo "<li>".get_the_title()."</li>";
+								
+								$post_categories = get_the_category();
+								echo "<ul>";
+								foreach($post_categories as $cat){
+									echo "<li>".$cat->name."</li>";
+								}
+								echo "</ul>";
+							}
+						endwhile;
+						echo "</ul>";
+
+						restore_current_blog();
+						?>
+					</div>
+					<div class="column">
+						<span class="label">Registration date:</span>
+						<?php
+							$reg_date = new DateTime($userProfile->user_registered);
+							echo $reg_date->format('m/d/Y');
+						?>
+						<span class="label">Expiration date:</span>
+						<?php
+							$reg_date = new DateTime($userProfile->user_registered);
+							//echo $reg_date->format('m/d/Y');
+						?>
+					</div>
+				</div>
+			</div>
+			<div class="column">
+				<div class="bluebox">
+					<label><strong>Change Password</strong></label>
+					<?php if($pass_note) echo "<p style='color:#fff;'>".$pass_note."</p>"; ?>
+					<label><span>Old Password:</span> <input name="o_pass" type="password" value="" /></label>
+					<label><span>New Password:</span> <input name="n_pass" type="password" value="" /></label>
+					<label><span>Repeat New Password:</span> <input name="n_c_pass" type="password" value="" /></label>
+				</div>
+			</div>
+		</div>
 		
 		<hr>
 		
